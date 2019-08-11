303f6084334cee4085f4d22130f0bffac1a74b29
guoruihan <43490455+guoruihan@users.noreply.github.com>
2019-08-11 15:30:39 +0800

    Create userdef.go

---
+++ userdef.go
@@ -0,0 +1,8 @@
+package main
+
+import "test/chord" // whether you can work or not, submit to github like `test/bulabula`, when you want to be judged.
+
+// NewNode use your own node create method to overwrite
+func NewNode(port int) dhtNode {
+	return chord.NewNode(port)
+}
